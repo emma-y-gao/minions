443e8328e3febb34d23d846de3b89d0c595ca189
Salman Abdullah <salman01@stanford.edu>
2025-03-24 16:40:29 +0500

    added faiss-cpu, sentence_transformers, and torch to setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,9 @@ setup(
         "rank_bm25",  # for smart retrieval
         "PyMuPDF",  # for PDF handling
         "firecrawl-py",  # for scraping urls
+        "faiss-cpu", # for embedding search
+        "sentence-transformers", # for pretrained embedding models
+        "torch" # for running embedding models on CUDA
     ],
     extras_require={
         "mlx": ["mlx-lm"],
